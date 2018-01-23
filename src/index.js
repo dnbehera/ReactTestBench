4bcc53a12367c8d50a8b0b4ea9fa938d585f0c32
dnbehera <>
2018-01-23 09:58:27 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return(
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i){
+        return(
+            <Square value={i}/>
+        )
+    }
+    render(){
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare('X')}
+                    {this.renderSquare('O')}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(9)}  
+                    {this.renderSquare(9)}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(9)}  
+                {this.renderSquare(9)}
+                {this.renderSquare(9)}
+                </div>
+            </div>
+        )
+
+    }
+}
+
+ReactDOM.render(
+    <Board />,
+    document.getElementById("root")
+);
